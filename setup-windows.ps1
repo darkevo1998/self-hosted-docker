c3db7dadbce60ecb4aa4c331c456359b2efd4074
darkevo1998 <timotius_albert@yahoo.com>
2025-11-17 07:31:34 +0700

    chore: update Docker and setup scripts to remove Redis configuration
    - Changed port mapping in `docker-compose.yml` from exposing port 80 to mapping it to 8082.
    - Removed commented-out Redis configuration from `setup-unix.sh` and `setup-windows.ps1` scripts to clean up the codebase.

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -68,10 +68,6 @@ AP_POSTGRES_PASSWORD=$postgresPassword
 AP_POSTGRES_HOST=postgres
 AP_POSTGRES_PORT=5432
 
-# Redis Configuration
-AP_REDIS_HOST=redis
-AP_REDIS_PORT=6379
-
 # Security
 AP_JWT_SECRET=$jwtSecret
 ENCRYPTION_KEY=$encryptionKey
